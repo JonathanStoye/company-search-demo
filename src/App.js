9490e07679d20bc06b5f8df52011f45614e3f827
Jonathan Stoye <mail@jonathanstoye.de>
2019-06-07 23:01:40 +0200

    feat: cleanup and basic rendering
    Signed-off-by: Jonathan Stoye <mail@jonathanstoye.de>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import {
+  Card,
+  CardImg,
+  CardBlock,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  Row,
+  Col,
+} from '@bootstrap-styled/v4'
+import { getCompanies } from './services/companyService'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [companies, setCompanies] = useState([])
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const { data } = await getCompanies()
+        setCompanies(data)
+      } catch (error) {}
+    }
+    fetchCompanies()
+  }, [setCompanies])
+
+  const companiesCards = companies.map(
+    ({ companyName, logo, specialty, city }) => (
+      <Col className="col-sm-4" key={logo} style={{ marginBottom: 30 }}>
+        <Card>
+          <CardImg
+            top
+            src="http://placeimg.com/640/360/arch"
+            alt="Card image cap"
+          />
+          <CardBlock>
+            <CardTitle>{companyName}</CardTitle>
+            <CardSubtitle>{city}</CardSubtitle>
+            <CardText>{specialty}</CardText>
+            <Button color="primary">Get in touch</Button>
+          </CardBlock>
+        </Card>
+      </Col>
+    )
+  )
+
+  return <Row>{companiesCards}</Row>
 }
 
-export default App;
+export default App
